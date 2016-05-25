09d04fcc000b38000b1ebc2d31d721ee87bb9d80
Pablo <pablokintopp@gmail.com>
2016-05-25 12:34:08 -0300

    EventHandler DataGridView testes (andamento)

--- Savage Hotel System/Savage Hotel System/Views/Func_List.Designer.cs
+++ Savage Hotel System/Savage Hotel System/Views/Func_List.Designer.cs
@@ -30,9 +30,6 @@
         {
             this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.databaseHotelDataSet1 = new Savage_Hotel_System.DatabaseHotelDataSet1();
-            this.funcionarioBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.funcionarioTableAdapter = new Savage_Hotel_System.DatabaseHotelDataSet1TableAdapters.FuncionarioTableAdapter();
             this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.loginDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -43,14 +40,18 @@
             this.genderDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.phoneDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.birthdayDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.funcionarioBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.databaseHotelDataSet1 = new Savage_Hotel_System.DatabaseHotelDataSet1();
+            this.funcionarioTableAdapter = new Savage_Hotel_System.DatabaseHotelDataSet1TableAdapters.FuncionarioTableAdapter();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.databaseHotelDataSet1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.funcionarioBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseHotelDataSet1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -66,26 +67,14 @@
             this.phoneDataGridViewTextBoxColumn,
             this.birthdayDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.funcionarioBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 121);
+            this.dataGridView1.Location = new System.Drawing.Point(8, 79);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(1155, 297);
+            this.dataGridView1.Size = new System.Drawing.Size(770, 193);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // databaseHotelDataSet1
-            // 
-            this.databaseHotelDataSet1.DataSetName = "DatabaseHotelDataSet1";
-            this.databaseHotelDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // funcionarioBindingSource
-            // 
-            this.funcionarioBindingSource.DataMember = "Funcionario";
-            this.funcionarioBindingSource.DataSource = this.databaseHotelDataSet1;
-            // 
-            // funcionarioTableAdapter
-            // 
-            this.funcionarioTableAdapter.ClearBeforeFill = true;
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEnter);
             // 
             // idDataGridViewTextBoxColumn
             // 
@@ -93,83 +82,98 @@
             this.idDataGridViewTextBoxColumn.HeaderText = "Id";
             this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
             this.idDataGridViewTextBoxColumn.ReadOnly = true;
-            this.idDataGridViewTextBoxColumn.Width = 59;
+            this.idDataGridViewTextBoxColumn.Width = 41;
             // 
             // nameDataGridViewTextBoxColumn
             // 
             this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
             this.nameDataGridViewTextBoxColumn.HeaderText = "Nome";
             this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
-            this.nameDataGridViewTextBoxColumn.Width = 87;
+            this.nameDataGridViewTextBoxColumn.Width = 60;
             // 
             // loginDataGridViewTextBoxColumn
             // 
             this.loginDataGridViewTextBoxColumn.DataPropertyName = "Login";
             this.loginDataGridViewTextBoxColumn.HeaderText = "Login";
             this.loginDataGridViewTextBoxColumn.Name = "loginDataGridViewTextBoxColumn";
-            this.loginDataGridViewTextBoxColumn.Width = 84;
+            this.loginDataGridViewTextBoxColumn.Width = 58;
             // 
             // passwordDataGridViewTextBoxColumn
             // 
             this.passwordDataGridViewTextBoxColumn.DataPropertyName = "Password";
             this.passwordDataGridViewTextBoxColumn.HeaderText = "Senha";
             this.passwordDataGridViewTextBoxColumn.Name = "passwordDataGridViewTextBoxColumn";
-            this.passwordDataGridViewTextBoxColumn.Width = 92;
+            this.passwordDataGridViewTextBoxColumn.Width = 63;
             // 
             // cPFDataGridViewTextBoxColumn
             // 
             this.cPFDataGridViewTextBoxColumn.DataPropertyName = "CPF";
             this.cPFDataGridViewTextBoxColumn.HeaderText = "CPF";
             this.cPFDataGridViewTextBoxColumn.Name = "cPFDataGridViewTextBoxColumn";
-            this.cPFDataGridViewTextBoxColumn.Width = 76;
+            this.cPFDataGridViewTextBoxColumn.Width = 52;
             // 
             // salaryDataGridViewTextBoxColumn
             // 
             this.salaryDataGridViewTextBoxColumn.DataPropertyName = "Salary";
             this.salaryDataGridViewTextBoxColumn.HeaderText = "Salario";
             this.salaryDataGridViewTextBoxColumn.Name = "salaryDataGridViewTextBoxColumn";
-            this.salaryDataGridViewTextBoxColumn.Width = 94;
+            this.salaryDataGridViewTextBoxColumn.Width = 64;
             // 
             // isManagerDataGridViewTextBoxColumn
             // 
             this.isManagerDataGridViewTextBoxColumn.DataPropertyName = "IsManager";
             this.isManagerDataGridViewTextBoxColumn.HeaderText = "Cargo";
             this.isManagerDataGridViewTextBoxColumn.Name = "isManagerDataGridViewTextBoxColumn";
-            this.isManagerDataGridViewTextBoxColumn.Width = 88;
+            this.isManagerDataGridViewTextBoxColumn.Width = 60;
             // 
             // genderDataGridViewTextBoxColumn
             // 
             this.genderDataGridViewTextBoxColumn.DataPropertyName = "Gender";
             this.genderDataGridViewTextBoxColumn.HeaderText = "Genero";
             this.genderDataGridViewTextBoxColumn.Name = "genderDataGridViewTextBoxColumn";
-            this.genderDataGridViewTextBoxColumn.Width = 99;
+            this.genderDataGridViewTextBoxColumn.Width = 67;
             // 
             // phoneDataGridViewTextBoxColumn
             // 
             this.phoneDataGridViewTextBoxColumn.DataPropertyName = "Phone";
             this.phoneDataGridViewTextBoxColumn.HeaderText = "Telefone";
             this.phoneDataGridViewTextBoxColumn.Name = "phoneDataGridViewTextBoxColumn";
-            this.phoneDataGridViewTextBoxColumn.Width = 107;
+            this.phoneDataGridViewTextBoxColumn.Width = 74;
             // 
             // birthdayDataGridViewTextBoxColumn
             // 
             this.birthdayDataGridViewTextBoxColumn.DataPropertyName = "Birthday";
             this.birthdayDataGridViewTextBoxColumn.HeaderText = "Data Nascimento";
             this.birthdayDataGridViewTextBoxColumn.Name = "birthdayDataGridViewTextBoxColumn";
-            this.birthdayDataGridViewTextBoxColumn.Width = 154;
+            this.birthdayDataGridViewTextBoxColumn.Width = 105;
+            // 
+            // funcionarioBindingSource
+            // 
+            this.funcionarioBindingSource.DataMember = "Funcionario";
+            this.funcionarioBindingSource.DataSource = this.databaseHotelDataSet1;
+            // 
+            // databaseHotelDataSet1
+            // 
+            this.databaseHotelDataSet1.DataSetName = "DatabaseHotelDataSet1";
+            this.databaseHotelDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // funcionarioTableAdapter
+            // 
+            this.funcionarioTableAdapter.ClearBeforeFill = true;
             // 
             // Func_List
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1167, 479);
+            this.ClientSize = new System.Drawing.Size(778, 314);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Func_List";
             this.Text = "Func_List";
             this.Load += new System.EventHandler(this.Func_List_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.databaseHotelDataSet1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.funcionarioBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.databaseHotelDataSet1)).EndInit();
             this.ResumeLayout(false);
 
         }
--- Savage Hotel System/Savage Hotel System/Views/Func_List.cs
+++ Savage Hotel System/Savage Hotel System/Views/Func_List.cs
@@ -13,6 +13,7 @@ namespace Savage_Hotel_System.Views
     public partial class Func_List : Form
     {
         private Func_Menu func_Menu;
+        private string cellLastValue;
 
         public Func_List()
         {
@@ -23,6 +24,8 @@ namespace Savage_Hotel_System.Views
         {
             InitializeComponent();
             this.func_Menu = func_Menu;
+            //CARREGA UM EVENTHANDLER NO LOAD
+            this.Load += new EventHandler(Func_List_Load);
         }
 
         private void Func_List_Load(object sender, EventArgs e)
@@ -30,16 +33,43 @@ namespace Savage_Hotel_System.Views
             // TODO: This line of code loads data into the 'databaseHotelDataSet1.Funcionario' table. You can move, or remove it, as needed.
             this.funcionarioTableAdapter.Fill(this.databaseHotelDataSet1.Funcionario);
 
+            //Linkando um evento para lidar com mudanças nos campos da DataGridView
+            this.dataGridView1.CellValidating += new
+            DataGridViewCellValidatingEventHandler(dataGridView1_CellValidating);
+            this.dataGridView1.CellEndEdit += new DataGridViewCellEventHandler(dataGridView1_CellEndEdit);
+                      
+        }
 
+        private void dataGridView1_CellValidating(object sender,
+        DataGridViewCellValidatingEventArgs e)
+        {
+            //nome da coluna
+            string headerText =
+                dataGridView1.Columns[e.ColumnIndex].HeaderText;
+
+            string cellValue = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString();
+            if(e.ColumnIndex > 0)
+                dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.BackColor = Color.Red;
+
+            //TODO IMPEDIR A MUDANCA NO BANCO SEM A PERMIÇAO DESTE METÓDO
+            //TODO DESATIVAR BINDING com o bancoAUTOMATICO
+            //TODO Verficar nome/index da Coluna e validade de acordo e validar de acordo
+            // MessageBox.Show("Validando "+headerText);
+        }
+        void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.BackColor = Color.White;
         }
 
         private void funcionarioBindingSource_CurrentChanged(object sender, EventArgs e)
         {
                     }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
         {
-
+            //SALVADO TEMPORARIAMENTO O VALOR DE CADA CELULA QUE ESTEJA EM FOCO
+            //Para CASO a pessoa desista de editar a coluna            
+            cellLastValue = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString();            
         }
     }
 }
